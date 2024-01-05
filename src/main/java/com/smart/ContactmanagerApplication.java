09bdb8c162b7d0b38b4c431a72d88f3b9bb027ac
Devendra6536 <devendrapal8682@gmail.com>
2024-01-05 13:07:43 +0530

    changed

--- src/main/java/com/smart/ContactmanagerApplication.java
+++ src/main/java/com/smart/ContactmanagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ContactmanagerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello engineer");
 		SpringApplication.run(ContactmanagerApplication.class, args);
 	}
 
