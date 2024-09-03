84b12d4521c67ca92474a50c229fa52c0054ee86
Devendra <devendra.kumar@cavisson.com>
2024-09-03 16:10:08 +0530

    Revert "Git revert for reverting the commit after pushing"
    This reverts commit f693cea08797d8a1a6c1721095ba058879a9adf4.

--- src/main/java/com/smart/helper/Greet.java
+++ src/main/java/com/smart/helper/Greet.java
@@ -3,6 +3,5 @@ public class Greet{
     public static void main(String[] args) {
         System.err.println("Hello how are you");
         System.out.print("This is used for learning the git purpose");
-        System.err.println("I want to learn the git revert");
     }
 }
